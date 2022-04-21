5868b302ac11ea23cb2e090b0b99f112b3cfe15b
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-04-21 14:32:43 -0300

    Exercise while

---
+++ Assets/Scripts/While01.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While01 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los números son iguales");
+        }
+
+        else if (num1 < num2)
+        {
+            while (num1 <= num2)
+            {
+                Debug.Log(num1);
+                num1++;
+            }
+        }
+
+        else
+        {
+            while (num1 >= num2)
+            {
+                Debug.Log(num1);
+                num1--;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
